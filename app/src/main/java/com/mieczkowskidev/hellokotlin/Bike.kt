c327a72e5f57979767bb96ea9db1c5f4e058ea97
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2016-12-12 22:36:09 +0100

    getting started

---
+++ app/src/main/java/com/mieczkowskidev/hellokotlin/Bike.kt
@@ -0,0 +1,7 @@
+package com.mieczkowskidev.hellokotlin
+
+/**
+ * Created by Patryk Mieczkowski on 2016-12-12
+ */
+
+class Bike(val id: String, val size: Int)
--- app/src/main/java/com/mieczkowskidev/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/mieczkowskidev/hellokotlin/MainActivity.kt
@@ -5,8 +5,26 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val x: Int = 1
+    var y: Int = 2
+    val rower: Bike = Bike("asd", 23)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        print(rower.id + rower.size)
+
     }
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun lista(mojaLista: Array<String>) {
+        if (mojaLista.isEmpty()) return
+
+        print("First argument: ${mojaLista[0]}")
+    }
+
 }
